a5bac82be5cf352808d3ed9ab2a89737579a1498
Lowell Mower <lowell.m.mower@gmail.com>
2018-05-06 16:39:45 -0400

    connect currently just lists containers

--- docker/src/client.rs
+++ docker/src/client.rs
@@ -7,9 +7,11 @@ pub struct Client {
 }
 
 impl Client {
-    pub fn connect_default(self) {
+    pub fn connect(self) {
         let mut stream = UnixStream::connect(self.connection).unwrap();
-        stream.write_all(b"GET /events HTTP/1.0\r\n\r\n").unwrap();
+        stream
+            .write_all(b"GET /containers/json HTTP/1.0\r\n\r\n")
+            .unwrap();
         let mut response = String::new();
         stream.read_to_string(&mut response).unwrap();
         println!("{}", response);
