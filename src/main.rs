93df0de38a611f0295fbff879e16c620b48c0cf8
Lowell Mower <lowell.m.mower@gmail.com>
2018-05-06 16:42:46 -0400

    add file line parse to clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 extern crate ship_wreck;
+extern crate clap;
+
+use clap::{Arg, App};
 
 fn main() {
-    // let c = ship_wreck::get_default_client();
-    // c.connect_default();
-    ship_wreck::load_config();
-    // ship_wreck::connect_client();
+    let sw_args = App::new("Ship Wreck")
+        .version("0.0.1")
+        .author("Lowell M. <lowell.mower@gmail.com>")
+        .about("Health monitoring for Docker containers")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let config_file = sw_args.value_of("config").unwrap_or("default.yml");
+    let client = ship_wreck::get_client_from_file(config_file);
+    println!("{:?}", client);
+    println!("Connecting client...\n");
+    client.connect();
 }
